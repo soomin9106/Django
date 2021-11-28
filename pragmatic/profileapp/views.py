06466ece00c73a87b54848fa427a1cf115e716d2
soomin9106 <soomin9106@naver.com>
2021-11-28 19:30:39 +0900

    profile update view and decorators

--- pragmatic/profileapp/views.py
+++ pragmatic/profileapp/views.py
@@ -5,8 +5,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView
+from profileapp.decorator import profile_ownership_required
 from profileapp.models import Profile
 from profileapp.forms import ProfileCreationForm
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+has_ownership = [profile_ownership_required, login_required]
 
 
 class ProfileCreateView(CreateView):
@@ -21,3 +28,13 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+
+
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
