05330fd0cc0d015e709a7b2f0a0c612dd1e76540
soomin9106 <soomin9106@naver.com>
2021-11-28 19:08:01 +0900

    profile create view

--- pragmatic/profileapp/views.py
+++ pragmatic/profileapp/views.py
@@ -1,3 +1,23 @@
+from django.db import models
+from django.forms import forms
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from profileapp.models import Profile
+from profileapp.forms import ProfileCreationForm
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'taret_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        temp_profile = form.save(commit = False) #우리가 보낸 form => 임시 대기
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        return super().form_valid(form)
