03131960a04cf4017b08db41ccccccfaf76bb7d1
soomin9106 <soomin9106@naver.com>
2021-11-30 17:17:03 +0900

    subsrciption function

--- pragmatic/pragmatic/settings.py
+++ pragmatic/pragmatic/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'articleapp',
     'commentapp',
     'projectapp',
+    'subscribeapp',
 ]
 
 MIDDLEWARE = [
---
+++ pragmatic/subscribeapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import get_object_or_404, render
+from django.urls.base import reverse
+from django.views.generic import RedirectView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from projectapp.models import Project
+from subscribeapp.models import Subscription
+
+# Create your views here.
+
+
+@method_decorator(login_required,'get')
+class SubscriptionView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('projectapp:detail',kwargs={'pk':self.request.GET.get('project_pk')})
+    
+    def get(self, request, *args, **kwargs):
+        project = get_object_or_404(Project, pk=self.request.GET.get('project_pk'))
+        user = self.request.user
+
+        subscription = Subscription.objects.filter(user=user,project=project)
+
+
+        if subscription.exists():
+            subscription.delete()
+        else:
+            Subscription(user=user,project=project).save()
+
+
+        return super(SubscriptionView,self).get(self,request,*args,**kwargs)
+
+
