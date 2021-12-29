43e3e934aedd6c9a33ec3c38125bd6b27ddc2c9b
soomin9106 <soomin9106@naver.com>
2021-12-29 10:46:28 +0900

    css and js

---
+++ pragmatic/static/js/base.js
@@ -0,0 +1,13 @@
+//loading select search function library
+$('#platform-select').select2(
+    {
+        placeholder: '플랫폼을 선택하세요.'
+    }
+);
+$('#target-select').select2(
+    {
+        placeholder: '지표를 선택하세요.'
+    }
+);
+
+//tabulator
